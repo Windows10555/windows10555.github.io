01690f1272974e9a1a23f996665d2cb38adceace
伊琳鹜 <Windows10555@163.com>
2024-07-16 13:24:24 +0800

    Try to fix moment load error

--- js/time.js
+++ js/time.js
@@ -13,7 +13,7 @@ function updateTimestamp() {
     // 更新显示
     document.getElementById('htmer_time').innerHTML = `${duration.years()}年${duration.months()}月${duration.days()}天${duration.hours()}时${duration.minutes()}分${duration.seconds()}秒`;
 }
-
+setTimeout(2000);
 // 立即执行一次updateTimestamp函数，然后每隔一秒调用一次
 updateTimestamp();
 setInterval(updateTimestamp, 1000);
