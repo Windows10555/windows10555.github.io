d8b54573086ad81c62bfe65ef4311c18a84433e9
Irin Wakako <Windows10555@163.com>
2025-08-19 15:39:07 +0800

    加载优化+BF
    我知道服务器还是炸的

--- js/time.js
+++ js/time.js
@@ -1,19 +1,11 @@
 function updateTimestamp() {
-    // 设置moment.js为中文locale
     moment.locale('zh-cn');
-
-    // 设置起始日期
     var startDate = moment("2019-07-02");
-    // 获取当前日期
     var currentDate = moment();
-    // 计算两个日期之间的差异
     var difference = currentDate.diff(startDate, true);
     var duration = moment.duration(difference);
-
-    // 更新显示
     document.getElementById('htmer_time').innerHTML = `${duration.years()}年${duration.months()}月${duration.days()}天${duration.hours()}时${duration.minutes()}分${duration.seconds()}秒`;
 }
 setTimeout(2000);
-// 立即执行一次updateTimestamp函数，然后每隔一秒调用一次
 updateTimestamp();
 setInterval(updateTimestamp, 1000);
